cdc17a9e2ae16727c4a50157f02db68d22971b9e
Jieyi Long (Sliver) <jieyi@sliver.tv>
2020-05-20 18:06:10 -0700

    Update OriginalGN.sol

--- docs/smart-contract-examples/OriginalGN.sol
+++ docs/smart-contract-examples/OriginalGN.sol
@@ -104,7 +104,7 @@ contract OriginalGN is StandardToken {
     Some wallets/interfaces might not even bother to look at this information.
     */
     string public name = "OriginalGN";                   //fancy name: eg Simon Bucks
-    uint8 public decimals = 2;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    uint8 public decimals = 0;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol = "OGN";                 //An identifier: eg SBX
     string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
